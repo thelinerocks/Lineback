c19f7e7acfcac0bd3591ae2815cce9462ee99365
Charith Amarasinghe <ca508@ic.ac.uk>
2018-01-28 02:54:11 +0000

    Update instagram fetcher

--- instagram.py
+++ instagram.py
@@ -6,6 +6,13 @@ import json
 import logging
 import time
 import urllib.parse as urlparse
+import datetime
+import sys
+import base64
+import time
+
+import database
+
 
 logger = logging.getLogger("lineback.instagram")
 
@@ -34,22 +41,48 @@ def get_instagram_posts(tag, since, host="http://localhost:3000", limit=20):
         if len(output) > limit:
             break
 
-        ts = p["taken_at_timestamp"]
-        if ts < since:
+        ts = datetime.datetime.utcfromtimestamp(p["taken_at_timestamp"])
+        if ts <= since:
             break
 
         d = {}
+        d["date"] = ts
         if ("edge_media_to_caption" in p) and (p["edge_media_to_caption"]["edges"]):
-            d["text"] = p["edge_media_to_caption"]["edges"][0]["node"]["text"]
+            d["message"] = p["edge_media_to_caption"]["edges"][0]["node"]["text"]
         else:
-            d["text"] = ""
-        d["url"] = p["display_url"]
+            d["message"] = ""
+        d["image_url"] = p["display_url"]
 
-        d["thumb_src"] = ""
+        d["profile_pic_url"] = ""
         if "thumbnail_resources" in p:
-            d["thumb_src"] = p["thumbnail_resources"][0]["src"]
+            d["profile_pic_url"] = p["thumbnail_resources"][0]["src"]
+
+        d["social_network"] = "instagram"
+        d["user_name"] = "Instagram User"
         output.append(d)
     return output
 
 if __name__=="__main__":
-    get_instagram_posts("theline", time.time()-(60*60*48), host="http://108.61.175.107:3000", limit=20)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    t = database.get_most_recent("instagram")
+    if t is None:
+        t = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+    logger.info("Polling data since: %s", t.isoformat())
+
+    last = t
+    while True:
+        l = get_instagram_posts(sys.argv[1], last, host="http://108.61.175.107:3000", limit=20)
+
+        for post in l:
+            database.save_post(post)
+            if post["date"] > last:
+                last = post["date"]
+
+        logger.info("Got %d posts from instagram up to %s", len(l), last.isoformat())
+        time.sleep(5)
